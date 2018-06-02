62d46a9002af9eb5d32472be93fd06bb7d9e90f2
Milan Gatyas <Milan.Gatyas@massive.co>
2018-06-02 23:45:59 +0200

    init

---
+++ index.ts
@@ -0,0 +1,71 @@
+export interface Logger {
+    log(log: string | object): void;
+    logAsync(log: string | object): Promise<void>;
+    error(log: string | object): void;
+    errorAsync(log: string | object): Promise<void>;
+}
+
+export interface LogExtensionFunc {
+    (log: object, data?: any): object;
+}
+
+export class LoggerOptions {
+    readonly logExtensionFunc? : LogExtensionFunc;
+
+    constructor(logExtensionFunc?: LogExtensionFunc) {
+        this.logExtensionFunc = logExtensionFunc;
+    }
+}
+
+export interface LogManager {
+    init(loggerOptions: LoggerOptions): void;
+    createLogger(data?: any): Logger;
+}
+
+class ConsoleLogger implements Logger {
+    private readonly logExtensionFunc: LogExtensionFunc;
+    private readonly data?: any;
+
+    constructor(logExtensionFunc: LogExtensionFunc, data?: any) {
+        this.logExtensionFunc = logExtensionFunc;
+        this.data = data;
+    }
+
+    private serializeLog(log: string | object) {
+        const rawLog = typeof log === "string" ? {log} : log;
+        const extendedLog = this.logExtensionFunc(rawLog, this.data);
+        const serializedLog = JSON.stringify(extendedLog);
+        
+        return serializedLog;
+    }
+
+    log(log: string | object): void {
+        console.log(this.serializeLog(log));
+    }
+
+    logAsync(log: string | object): Promise<void> {
+        this.log(log);
+        return Promise.resolve();
+    }
+
+    error(log: string | object): void {
+        console.error(this.serializeLog(log));
+    }
+
+    errorAsync(log: string | object): Promise<void> {
+        this.error(log);
+        return Promise.resolve();
+    }
+}
+
+let logExtensionFunc: LogExtensionFunc = (log, data) => log;
+
+const deafultExport: LogManager = ({
+    init: (loggerOptions: LoggerOptions) => {
+        if (loggerOptions && loggerOptions.logExtensionFunc)
+            logExtensionFunc = loggerOptions.logExtensionFunc;
+    },
+    createLogger: (data?: any): Logger => new ConsoleLogger(logExtensionFunc, data)
+});
+
+export default deafultExport;
---
+++ readme.md
@@ -0,0 +1,11 @@
+# simple-logger
+Simple logger for nodejs application
+
+## Features
+Logs are serialized as JSON objects
+Possibility to extend log object by custom properties by injection
+
+## Installation
+```sh
+npm install simple-logger
+```
